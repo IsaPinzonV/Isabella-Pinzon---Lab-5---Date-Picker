2d3213f7260cd402fdcc1c7cb61df0092c02c6c0
IsaPinzonV <isabellapv0305@outlook.com>
2022-09-09 11:49:32 -0500

    Cambios calendario base

--- script.js
+++ script.js
@@ -1,29 +1,61 @@
-const date_picker_element = document.querySelector('.date-picker');
-const selected_date_element = document.querySelector('.date-picker .selected-date');
-const dates_element = document.querySelector('.date-picker .dates');
-const mth_element = document.querySelector('.date picker .dates .month .next-mth');
-const next_mth = document.querySelector('.date picker .dates .month .prev-mth');
+const date_picker_element = document.querySelector(".date-picker");
+const selected_date_element = document.querySelector(
+  ".date-picker .selected-date"
+);
+const dates_element = document.querySelector(".date-picker .dates");
+const mth_element = document.querySelector(".date picker .dates .month .mth");
+const next_mth_element = document.querySelector(
+  ".date picker .dates .month .next-mth"
+);
+const prev_mth_element = document.querySelector(
+  ".date picker .dates .month .prev-mth"
+);
+const days_element = document.querySelector(".date picker .dates .days");
 
-const days_element = document.querySelector('.date picker .dates .days');
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "Jun",
+  "July",
+  "Auguts",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
-const months = ['January','February','March', 'April', 'May', 'Jun', 'July',  Auguts, ]
-date_picker_element.addEventListener('click', toggleDatePicker);
+let date = new Date();
+let day = date.getDate();
+let month = date.getMonth();
+let year = date.getFullYear();
 
-function toggleDatePicker (e) {
+let selectedDate = date;
+let selectedDay = day;
+let selectedMonth = month;
+let selectedYear = year;
 
-    console.log(e.path);
-	if (!checkEventPathForClass(e.path, 'dates')) {
-		dates_element.classList.toggle('active');
-	}
+console.log(months[month] + " " + year);
+//mth_element.textContent = months[month] + "" + year;
+
+date_picker_element.addEventListener("click", toggleDatePicker);
+
+function toggleDatePicker(e) {
+  console.log(e.path);
+
+  if (!checkEventPathForClass(e.path, "dates")) {
+    dates_element.classList.toggle("active");
+  }
 }
 
-function checkEventPathForClass (path, selector) {
-	for (let i = 0; i < path.length; i++) {
-		if (path[i].classList && path[i].classList.contains(selector)) {
-			return true;
-		}
-	}
-	
-	return false;
-}
+function checkEventPathForClass(path, selector) {
+  for (let i = 0; i < path.length; i++) {
+    if (path[i].classList && path[i].classList.contains(selector)) {
+      return true;
+    }
+  }
 
+  return false;
+}
